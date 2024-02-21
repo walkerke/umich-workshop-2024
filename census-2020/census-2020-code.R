24b1d139d1d1b300311bc600e7da2cd08a528895
Kyle Walker <kyle@walker-data.com>
2024-02-21 15:49:29 -0600

    publish workshop code

---
+++ census-2020/census-2020-code.R
@@ -0,0 +1,280 @@
+library(tidycensus)
+options(tigris_use_cache = TRUE)
+
+## install.packages(c("tidycensus", "tidyverse", "mapview"))
+
+## library(tidycensus)
+## 
+## census_api_key("YOUR KEY GOES HERE", install = TRUE)
+
+pop20 <- get_decennial(
+  geography = "state",
+  variables = "P1_001N",
+  year = 2020
+)
+
+pop20
+
+table_p2 <- get_decennial(
+  geography = "state", 
+  table = "P2", 
+  year = 2020
+)
+
+table_p2
+
+tx_county_pop <- get_decennial(
+  geography = "county",
+  variables = "P1_001N",
+  state = "TX",
+  sumfile = "dhc",
+  year = 2020
+)
+
+tx_county_pop
+
+matagorda_blocks <- get_decennial(
+  geography = "block",
+  variables = "P1_001N",
+  state = "TX",
+  county = "Matagorda",
+  sumfile = "dhc",
+  year = 2020
+)
+
+matagorda_blocks
+
+## vars <- load_variables(2020, "dhc")
+## 
+## View(vars)
+## 
+
+single_year_age <- get_decennial(
+  geography = "state",
+  table = "PCT12",
+  year = 2020,
+  sumfile = "dhc"
+)
+
+
+single_year_age
+
+single_year_age_wide <- get_decennial(
+  geography = "state",
+  table = "PCT12",
+  year = 2020,
+  sumfile = "dhc",
+  output = "wide" 
+)
+
+single_year_age_wide
+
+ca_samesex <- get_decennial(
+  geography = "county",
+  state = "CA",
+  variables = c(married = "DP1_0116P",
+                partnered = "DP1_0118P"),
+  year = 2020,
+  sumfile = "dp",
+  output = "wide"
+)
+
+ca_samesex
+
+library(tidyverse)
+
+tidyverse_logo()
+
+library(tidycensus)
+library(tidyverse)
+
+tx_population <- get_decennial(
+  geography = "county",
+  variables = "P1_001N",
+  year = 2020,
+  state = "TX",
+  sumfile = "dhc"
+)
+
+
+arrange(tx_population, value)
+
+
+arrange(tx_population, desc(value))
+
+below1000 <- filter(tx_population, value < 1000)
+
+below1000
+
+race_vars <- c(
+  Hispanic = "P5_010N",
+  White = "P5_003N",
+  Black = "P5_004N",
+  Native = "P5_005N",
+  Asian = "P5_006N",
+  HIPI = "P5_007N"
+)
+
+cd_race <- get_decennial(
+  geography = "congressional district",
+  variables = race_vars,
+  summary_var = "P5_001N", 
+  year = 2020,
+  sumfile = "cd118"
+)
+
+cd_race
+
+cd_race_percent <- cd_race %>%
+  mutate(percent = 100 * (value / summary_value)) %>% 
+  select(NAME, variable, percent) 
+
+cd_race_percent
+
+largest_group <- cd_race_percent %>%
+  group_by(NAME) %>% 
+  filter(percent == max(percent)) 
+
+# Optionally, use `.by`: 
+# largest_group <- cd_race_percent %>%
+#   filter(percent == max(percent), .by = NAME) 
+
+largest_group
+
+cd_race_percent %>%
+  group_by(variable) %>% 
+  summarize(median_pct = median(percent, na.rm = TRUE)) 
+
+iowa_over_65 <- get_decennial(
+  geography = "tract",
+  variables = "DP1_0024P",
+  state = "IA",
+  geometry = TRUE,
+  sumfile = "dp",
+  year = 2020
+)
+
+
+iowa_over_65
+
+library(mapview)
+
+mapview(iowa_over_65)
+
+mapview(iowa_over_65, zcol = "value")
+
+mapview(iowa_over_65, zcol = "value",
+        layer.name = "% age 65 and up<br>Census tracts in Iowa")
+
+
+library(viridisLite)
+
+mapview(iowa_over_65, zcol = "value",
+        layer.name = "% age 65 and up<br>Census tracts in Iowa",
+        col.regions = inferno(100))
+
+## library(htmlwidgets)
+## 
+## m1 <- mapview(iowa_over_65, zcol = "value",
+##         layer.name = "% age 65 and up<br>Census tracts in Iowa",
+##         col.regions = inferno(100))
+## 
+## saveWidget(m1@map, "iowa_over_65.html")
+## 
+
+mn_population_groups <- get_decennial(
+  geography = "state",
+  variables = "T01001_001N",
+  state = "MN",
+  year = 2020,
+  sumfile = "ddhca",
+  pop_group = "all",
+  pop_group_label = TRUE
+)
+
+
+mn_population_groups
+
+available_groups <- get_pop_groups(2020, "ddhca")
+
+get_decennial(
+  geography = "county",
+  variables = "T02001_001N",
+  state = "MN",
+  county = "Hennepin",
+  pop_group = "1325",
+  year = 2020,
+  sumfile = "ddhca"
+)
+
+check_ddhca_groups(
+  geography = "county", 
+  pop_group = "1325", 
+  state = "MN", 
+  county = "Hennepin"
+)
+
+library(tidycensus)
+
+hennepin_somali <- get_decennial(
+  geography = "tract",
+  variables = "T01001_001N",
+  state = "MN",
+  county = "Hennepin",
+  year = 2020,
+  sumfile = "ddhca",
+  pop_group = "1325",
+  pop_group_label = TRUE,
+  geometry = TRUE
+)
+
+
+mapview(hennepin_somali, zcol = "value")
+
+somali_dots <- as_dot_density(
+  hennepin_somali,
+  value = "value",
+  values_per_dot = 25
+)
+
+mapview(somali_dots, cex = 0.01, layer.name = "Somali population<br>1 dot = 25 people",
+        col.regions = "navy", color = "navy")
+
+county_pop_10 <- get_decennial(
+  geography = "county",
+  variables = "P001001", 
+  year = 2010,
+  sumfile = "sf1"
+)
+
+
+county_pop_10
+
+county_pop_10_clean <- county_pop_10 %>%
+  select(GEOID, value10 = value) 
+
+county_pop_10_clean
+
+county_pop_20 <- get_decennial(
+  geography = "county",
+  variables = "P1_001N",
+  year = 2020,
+  sumfile = "dhc"
+) %>%
+  select(GEOID, NAME, value20 = value)
+
+county_joined <- county_pop_20 %>%
+  left_join(county_pop_10_clean, by = "GEOID") 
+
+county_joined
+
+county_change <- county_joined %>%
+  mutate( 
+    total_change = value20 - value10, 
+    percent_change = 100 * (total_change / value10) 
+  ) 
+
+
+county_change
+
+filter(county_change, is.na(value10))
